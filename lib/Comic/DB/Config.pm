ec52e5c9244ffd93cb35282ef443de08e476454a
Yuta YOSHIIKE <luckypool314@gmail.com>
2012-10-28 22:52:22 +0900

    add Comic::DB::Config

--- lib/Comic/DB/Config.pm
+++ lib/Comic/DB/Config.pm
@@ -2,4 +2,29 @@ package Comic::DB::Config;
 use strict;
 use warnings;
 
+use parent qw/Exporter/;
+
+my $constant;
+
+BEGIN {
+    $constant = {
+        DB_MAIN_MST => 'dbi:mysql:comic_main',
+        DB_MAIN_SLV => 'dbi:mysql:comic_main',
+        DB_MAGAZINE_MST => 'dbi:mysql:comic_magazize',
+        DB_MAGAZINE_SLV => 'dbi:mysql:comic_magazize',
+        DB_PUBLISHER_MST => 'dbi:mysql:comic_publisher',
+        DB_PUBLISHER_SLV => 'dbi:mysql:comic_publisher',
+        DB_BOOK_MST => 'dbi:mysql:comic_book',
+        DB_BOOK_SLV => 'dbi:mysql:comic_book',
+    };
+};
+
+use constant {
+    CONSTANT => $constant,
+    map { $_ => $constant->{$_} } keys %$constant,
+};
+
+our @EXPRT = keys %{CONSTANT()};
+our @EXPRT_OK = keys %{CONSTANT()};
+
 1;
---
+++ t/lib/Comic/DB/Config.t
@@ -0,0 +1,15 @@
+use strict;
+use warnings;
+
+use Test::More;
+use Test::MockObject;
+
+BEGIN {
+    use_ok 'Comic::DB::Config';
+};
+
+subtest q/constant/ => sub {
+    ok Comic::DB::Config::DB_MAIN_MST;
+};
+
+done_testing;
